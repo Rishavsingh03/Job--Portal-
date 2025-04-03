3cb47e897930ba86727652e43e035d619c0a9e0a
Rishavsingh03 <rishavkumar3864@gmail.com>
2025-04-03 19:29:12 +0530

    Cors correction

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://work-vista-frontend.onrender.com/',
+    origin:'https://work-vista-frontend.onrender.com',
     credentials:true
 }
 
